6e745be5ef819b56d31ecea3a72726e0decfcc40
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2025-08-30 18:28:23 +0300

    installed django toolbar, added veiws in core_app, made index view

--- core_app/views.py
+++ core_app/views.py
@@ -1,3 +1,11 @@
+from http.client import HTTPResponse
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        username = request.user.username
+        return HttpResponse(f"<p>Hello {username}!!! </p>")
+    return HttpResponse("<p>Hello Anonymous!!!</p>")
